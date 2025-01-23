5a6f0e728079565c61dc43d27692abf56cf02520
Damien Cuvillier <damien.cuvillier.37@gmail.com>
2025-01-23 10:50:57 +0100

    Modif de l'url

--- src/App.js
+++ src/App.js
@@ -14,7 +14,7 @@ import Distributeur from './Composants/Distributeur';
 
 function App() {
   return (
-    <Router basename="/sorigny">
+    <Router basename="/sorigny/">
       <ScrollToTop />
       <div className="flex flex-col min-h-screen bg-gray-100">
         <Navbar />
